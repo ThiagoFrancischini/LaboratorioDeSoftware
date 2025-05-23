d6a4f8d543dbc99290f54027e9e555e7559c6428
Thiago Schmitz <thiagofrancischinischmitz@gmail.com>
2025-05-22 23:49:46 -0300

    Conexao banco

--- LaboratorioDeSoftware/Program.cs
+++ LaboratorioDeSoftware/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"));
 });
     
 // Add services to the container.
@@ -13,11 +13,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
